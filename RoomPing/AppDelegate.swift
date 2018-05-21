e4b39a5aafddbf2d3a9f1f19740b0a27d678b8aa
Maciej Burda <maciej.burda@me.com>
2018-05-21 09:23:21 +0200

    added api_key and beacons for tests

--- RoomPing/AppDelegate.swift
+++ RoomPing/AppDelegate.swift
@@ -18,7 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         // Get your API_KEY at Matchmore.io
-        let config = MatchMoreConfig(apiKey: "API_KEY")
+        let config = MatchMoreConfig(apiKey: "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJhbHBzIiwic3ViIjoiMWNkMTVlMjEtOGQyOS00MTNiLWIwM2YtMTZmMWNiYjAxNTY4IiwiYXVkIjpbIlB1YmxpYyJdLCJuYmYiOjE1MjYxMzMxNDksImlhdCI6MTUyNjEzMzE0OSwianRpIjoiMSJ9.csdAaV0hI5InT_QIgwxZLYw5YpFgA05nFagnG2n-ujbiYJd7LJZ678sAySYndObr2UPGW0XjNqSE3tzQzxYmdw")
+        
         Matchmore.configure(config)
         Matchmore.startUsingMainDevice { result in
             switch result {
@@ -26,7 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 print("Using main device")
                 Matchmore.startPollingMatches(pollingTimeInterval: 5)
                 Matchmore.startRangingBeacons(updateTimeInterval: 5)
-                Matchmore.startUpdatingLocation()
+                // Matchmore.startUpdatingLocation()
             case let .failure(error):
                 print(error.debugDescription)
             }
